77cd9cec7d17f7433212dae3e882ac76af873b05
T-Jump <mondasteele@gmail.com>
2018-11-14 00:59:06 -0800

    Added HTML elements for editor options
    Still need to add the jQuery to make them work.

--- js/func-debug.js
+++ js/func-debug.js
@@ -2,6 +2,19 @@ $(function() {
     $("#gridToggle").change(function(){
 		showGrid = this.checked;
 	});
+	$("#main").click(function(e){
+		var x = Math.floor((e.pageX - this.offsetLeft) / 2);
+		var y = Math.floor((e.pageY - this.offsetTop) / 2);
+		var xIndex = Math.floor(x / arena.columnWidth);
+		var yIndex = Math.floor(y / arena.rowHeight);
+		var selectedTile = $("input[type='radio'][name='tiletype']:checked");
+		var currentTile = puzzle1.tiles[yIndex][xIndex];
+		if(currentTile != null) {
+			puzzle1.tiles[yIndex][xIndex] = null;
+		} else {
+		puzzle1.tiles[yIndex][xIndex] = new CosmicCube(0);
+		}
+	});
 });
 
 function debug() {
